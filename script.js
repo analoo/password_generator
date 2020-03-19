3852edab003d93a4a5c51da8a38ec33e14654817
Ana Medrano <amedrano.fernandez@gmail.com>
2020-03-18 22:19:45 -0700

    added logic for generating password based on used requirements like length and mix of char types

--- script.js
+++ script.js
@@ -18,69 +18,76 @@ function generatePassword() {
   // asks the user what length password they want
   var passwordLength = prompt("What is the length password that you need? (min 8 chars, max 128)");
 
-  if ( passwordLength >= 8 && passwordLength <= 128 )  {
-    joinChars(passwordRequirements(),passwordLength);
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    return joinChars(passwordRequirements(), passwordLength);
   }
 
   else {
     generatePassword();
   }
-  
-  if (true) {
-    password = "NiceTry!";
-    return password;
-  }
-  else {
-    alert("Try Again!")
-  }
 }
 
-function passwordRequirements(){
+function passwordRequirements() {
   var numbersBoolean = confirm("Do you want to include numbers?");
   var specialCharsBoolean = confirm("Do you want to include special characters?");
   var lowerCaseBoolean = confirm("Do you want to include lower case?");
   var upperCaseBoolean = confirm("Do you want to include upper case?");
   if (numbersBoolean || specialCharsBoolean || lowerCaseBoolean || upperCaseBoolean) {
-   var userOptions = [numbersBoolean, specialCharsBoolean , lowerCaseBoolean , upperCaseBoolean];
-   return userOptions
+    var userOptions = [numbersBoolean, specialCharsBoolean, lowerCaseBoolean, upperCaseBoolean];
+    return userOptions
   }
 
-  else{
+  else {
     alert("You didn't select any requirements!")
     passwordRequirements()
   }
 
 }
 
-function joinChars(requirements, length){
+
+function joinChars(requirements, length) {
   var joinedText = "";
-  for (let i =0; i < length; i++){
-    joinedText = joinedText + generateLowerCase();
+  var userOptionFunctions = [generateNumber, generateSpecialChar, generateLowerCase, generateUpperCase];
+  var textGenerator = [];
+
+  for (let i=0; i<requirements.length; i++){
+    if(requirements[i]){
+      textGenerator.push(userOptionFunctions[i]);
+
+    }
   }
+  for (let i = 0; i < length; i++) {
+    var generator = textGenerator[i%textGenerator.length];
+    joinedText += generator();
+  }
+
+  return joinedText;
+
   console.log(joinedText);
+
 }
 
 // function that creates a lower case letter when called upon
 function generateLowerCase() {
-  var value= Math.floor(Math.random() * (122 - 97) + 97);
+  var value = Math.floor(Math.random() * (122 - 97) + 97);
   return String.fromCharCode(value);
 }
 
 // function that creates an upper case letter when called upon
 function generateUpperCase() {
-  var value= Math.floor(Math.random() * (90 - 65) + 65);
+  var value = Math.floor(Math.random() * (90 - 65) + 65);
   return String.fromCharCode(value);
 }
 
 // function that creates a number in string format
 function generateNumber() {
-  var value= Math.floor(Math.random() * (57 - 48) + 48);
+  var value = Math.floor(Math.random() * (57 - 48) + 48);
   return String.fromCharCode(value);
 }
 
 // function that creates a special character
 function generateSpecialChar() {
-  var value= Math.floor(Math.random() * (47 - 33) + 33);
+  var value = Math.floor(Math.random() * (47 - 33) + 33);
   return String.fromCharCode(value);
 }
 
