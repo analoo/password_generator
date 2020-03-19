efb2b22a95dc50fb8371b302da17a8f7ba9668b9
Ana Medrano <amedrano.fernandez@gmail.com>
2020-03-18 20:21:41 -0700

    added functions that return special characters and numbers

--- script.js
+++ script.js
@@ -18,11 +18,7 @@ function generatePassword() {
 
   // asks the user what length password they want
   var passwordLength = prompt("What is the length password that you need? (min 8 chars, max 128)");
-  var numbersBoolean = confirm("Do you want to include numbers?");
-  var specialCharsBoolean = confirm("Do you want to include special characters?");
-  var lowerCaseBoolean = confirm("Do you want to include lower case?");
-  var upperCaseBoolean = confirm("Do you want to include upper case?");
-  var userAnswers = [numbersBoolean, specialCharsBoolean, lowerCaseBoolean, upperCaseBoolean];
+
 
 
   console.log(lowerCaseBoolean)
@@ -34,24 +30,49 @@ function generatePassword() {
   else {
     alert("Try Again!")
   }
+}
 
+function passwordRequirements(){
+  var numbersBoolean = confirm("Do you want to include numbers?");
+  var specialCharsBoolean = confirm("Do you want to include special characters?");
+  var lowerCaseBoolean = confirm("Do you want to include lower case?");
+  var upperCaseBoolean = confirm("Do you want to include upper case?");
+  if (numbersBoolean || specialCharsBoolean || lowerCaseBoolean || upperCaseBoolean) {
+   var userOptions = [numbersBoolean, specialCharsBoolean , lowerCaseBoolean , upperCaseBoolean];
+   return userOptions
+  }
 
-
-
+  else{
+    alert("You didn't select any requirements!")
+    passwordRequirements()
+  }
 
 }
 
 // function that creates a lower case letter when called upon
 function generateLowerCase() {
-  var lcValue = Math.floor(Math.random() * (122 - 97) + 97);
-  return String.fromCharCode(lcValue);
+  var value= Math.floor(Math.random() * (122 - 97) + 97);
+  return String.fromCharCode(value);
 }
 
-
+// function that creates an upper case letter when called upon
 function generateUpperCase() {
-  var lcValue = Math.floor(Math.random() * (90 - 65) + 65);
-  return String.fromCharCode(lcValue);
+  var value= Math.floor(Math.random() * (90 - 65) + 65);
+  return String.fromCharCode(value);
 }
+
+// function that creates a number in string format
+function generateNumber() {
+  var value= Math.floor(Math.random() * (57 - 48) + 48);
+  return String.fromCharCode(value);
+}
+
+// function that creates a special character
+function generateSpecialChar() {
+  var value= Math.floor(Math.random() * (47 - 33) + 33);
+  return String.fromCharCode(value);
+}
+
 
 // console.log(generateUpperCase())
 
